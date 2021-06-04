3f50019ae9241704c8cd97c80086f1f8ef039f14
rakesh-tv <rakeshy@testvagrant.com>
2021-06-04 10:54:13 +0530

    Remove chromedriver and add log file

--- run.bat
+++ run.bat
@@ -1,2 +1,5 @@
 @ECHO OFF
-mvn clean test -DfolderPath=/Users/rakeshy/Desktop/sample -DwaitBetweenUpload=1
+For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
+For /f "tokens=1-2 delims=/:" %%a in ('time /t') do (set mytime=%%a%%b)
+
+mvn clean test -DfolderPath=C:\Users\rockm\Desktop\Newfolder\ -DwaitBetweenUpload=1 > Log_%mydate%_%mytime%.txt
